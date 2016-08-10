842f2a014c3616f41b8f6bc6fe9d0392addc7145
ydris <ydris@localhost.localdomain>
2016-08-10 12:02:43 -0400

    Create an ansible role for Jenkins on Windows (adapted from geerlingguy.ansible-role-jenkins)
    Started from geerlingguy.ansible-role-jenkins role and modify it to make it work on a windows target.
    The initial goal was to make the role compatible with both windows and linux but this is (I found) adding too many duplicate tasks (due to the fact windows and linux modules are never common)
    This role uses chocolatey as a package manager for windows and uses powershell commands (instead of linux shell).
    I added 2 groovy scripts: one to set the proxy and one to set the JLNP port (without this the CLI is disabled)
    I also changed the way to bypass the security wizard.
    
    Still some work to do on the configuration file (wich is totaly diferent than config files under linux) to set things like the jenkins port.

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -4,16 +4,15 @@ import jenkins.model.*
 
 def instance = Jenkins.getInstance()
 
-println "--> Checking if security has been set already"
+println "--> creating local user 'admin'"
 
-if (!instance.isUseSecurity()) {
-    println "--> creating local user 'admin'"
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
+instance.setSecurityRealm(hudsonRealm)
 
-    def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-    hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
-    instance.setSecurityRealm(hudsonRealm)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
 
-    def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-    instance.setAuthorizationStrategy(strategy)
-    instance.save()
-}
+
+
